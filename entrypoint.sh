1ab2db76bbc13bbf1b88f9eaf83d79e1a1798edd
Ernesto Alvarez <ernesto.alvarez@10pines.com>
2022-05-05 13:24:11 -0300

    prueba 4

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,4 @@ npm test
 apk --no-cache add curl
 curl -Os https://uploader.codecov.io/latest/linux/codecov
 chmod +x codecov
-CODECOV_TOKEN=37685e39-909f-4ec9-87b9-64e6a0ede2c1 ./codecov -R /..
+ ./codecov -t 37685e39-909f-4ec9-87b9-64e6a0ede2c1 -s ./coverage -R ./..
