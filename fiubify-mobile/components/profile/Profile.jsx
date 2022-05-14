5789a5e43b43b6432472786fb89b80a7d96d06d1
Ernesto Alvarez <ernesto.alvarez@10pines.com>
2022-05-14 16:50:50 -0300

    Añadido la conexion del perfil con el end-point de usuarios

---
+++ fiubify-mobile/components/profile/Profile.jsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+import { View, Text } from "react-native";
+import axios from "axios";
+
+export default function Profile({ userId: userUId }) {
+  const [user, setUser] = useState();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    getUser(userUId).then((user) => {
+      setUser(user);
+      setLoading(false);
+    });
+  }, [userUId]);
+
+  if (!loading) return <View>
+    <Text>Email: {user.email}</Text>
+    <Text>Role: {user.role}</Text>
+  </View>;
+  else return <View>
+    <Text>Loading</Text>
+  </View>;
+}
+
+async function getUser(userId) {
+  try {
+    let response = await axios.get(`https://fiubify-middleware-staging.herokuapp.com/user/${userId}`);
+    return response.data;
+  } catch (e) {
+    throw e;
+  }
+}
