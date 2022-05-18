f67b6ad935c7ef4ca8cd52a11eb1d5735b96a13f
Ernesto Alvarez <ernesto.alvarez@10pines.com>
2022-05-18 13:32:10 -0300

    Despues del login y la registración se redirreciona al perfil del usuario

--- fiubify-mobile/components/login/LoginForm.jsx
+++ fiubify-mobile/components/login/LoginForm.jsx
@@ -1,20 +1,20 @@
-import React, { Component, useState } from 'react'
-import { StyleSheet, Text, View, Button } from 'react-native'
+import React, { useState } from "react";
+import { StyleSheet, View } from "react-native";
 
-import { connect } from 'react-redux'
-import { bindActionCreators } from 'redux'
-import { logIn } from '../../state/actions/login.js'
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { logIn } from "../../state/actions/login.js";
 
-import UiTextInput from '../ui/UiTextInput.jsx'
-import UiButton from '../ui/UiButton.jsx'
-import { auth } from '../../firebase.js'
-import { signInWithEmailAndPassword } from 'firebase/auth'
+import UiTextInput from "../ui/UiTextInput.jsx";
+import UiButton from "../ui/UiButton.jsx";
+import { auth } from "../../firebase.js";
+import { signInWithEmailAndPassword } from "firebase/auth";
 
 function LoginForm({ actions, setUid }) {
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  return(
+  return (
     <View>
       <UiTextInput
         onChange={setEmail}
@@ -32,39 +32,40 @@ function LoginForm({ actions, setUid }) {
         onPress={() => send(email, password, actions.logIn, setUid)}
       />
     </View>
-  )
+  );
 
   async function send(email, password, logInAction, setUid) {
     signInWithEmailAndPassword(auth, email, password)
       .then(userCredentials => {
-        const user = userCredentials.user
+        const user = userCredentials.user;
+        setUid(user.uid);
       }).catch(error => {
-        alert(error.message)
-      })
-    }
+      alert(error.message);
+    });
   }
+}
 
 const mapStateToProps = state => {
-  return ({logged_in: state.loginState.logged_in})
-}
+  return ({ logged_in: state.loginState.logged_in });
+};
 
 const mapDispatchToProps = dispatch => {
-  return { actions: bindActionCreators({logIn}, dispatch) }
-}
+  return { actions: bindActionCreators({ logIn }, dispatch) };
+};
 
 const styles = StyleSheet.create({
   text_input: {
     marginBottom: 10,
   },
   link: {
-    fontWeight: 'bold',
-    color: 'blue',
+    fontWeight: "bold",
+    color: "blue",
     marginTop: 5,
-    marginBottom: 15
+    marginBottom: 15,
   },
   register_button: {
-    margin: 10
-  }
-})
+    margin: 10,
+  },
+});
 
-export default connect(mapStateToProps, mapDispatchToProps)(LoginForm)
+export default connect(mapStateToProps, mapDispatchToProps)(LoginForm);
