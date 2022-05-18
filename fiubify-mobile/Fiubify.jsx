20448fad2c09692f3a0a131df0b6abd9e8c1f2c0
Ernesto Alvarez <ernesto.alvarez@10pines.com>
2022-05-18 18:05:01 -0300

    Componen mainscreen para agregar las cosas necesarias

--- fiubify-mobile/Fiubify.jsx
+++ fiubify-mobile/Fiubify.jsx
@@ -1,23 +1,22 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import { connect } from "react-redux";
-
 import LoginScreen from "./components/login/Screen.jsx";
-import Profile from "./components/profile/Profile";
+import MainScreen from "./components/MainScreen";
 
-function LoginDispatcher(props) {
+function LoginDispatcher() {
   const [uid, setUid] = useState('')
   if (uid === '') {
     return <LoginScreen setUid={setUid} uid={uid}/>
   } else {
-    return <Profile userId={uid}></Profile> // App
+    return <MainScreen uid={uid}/> // App
   }
 }
 
-function Fiubify(props) {
+function Fiubify() {
   return (
     <View style={styles.container}>
-      <LoginDispatcher logged_in={props.logged_in} />
+      <LoginDispatcher />
     </View>
   );
 }
--- fiubify-mobile/components/MainScreen.jsx
+++ fiubify-mobile/components/MainScreen.jsx
@@ -1,4 +1,6 @@
+import Profile from "./profile/Profile";
+import React from "@types/react";
 
-export default function MainScreen() {
-
+export default function MainScreen({ uid }) {
+  return <Profile userId={uid}></Profile>;
 }
