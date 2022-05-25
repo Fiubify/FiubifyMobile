1709dd9b0ed66d48af344be667ec83682222ee43
Ernesto Alvarez <ernesto.alvarez@10pines.com>
2022-05-24 21:39:48 -0300

    App de class a function

--- fiubify-mobile/App.js
+++ fiubify-mobile/App.js
@@ -1,30 +1,26 @@
-import React, { Component } from "react";
-
-
-import { Provider } from 'react-redux';
-import configureStore from './state/store/configureStore.js';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import LoginScreen from './components/login/Screen';
-import Profile from './components/profile/Profile';
+import React from "react";
+import { Provider } from "react-redux";
+import configureStore from "./state/store/configureStore.js";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import LoginScreen from "./components/login/Screen";
+import Profile from "./components/profile/Profile";
 
 const store = configureStore();
 
 const Stack = createNativeStackNavigator();
 
-class App extends Component {
-  render() {
-    return (
-      <NavigationContainer>
-        <Provider store={store}>
-          <Stack.Navigator>
-            <Stack.Screen name="Login" component={LoginScreen} />
-            <Stack.Screen name="Profile" component={Profile} />
-          </Stack.Navigator>
-        </Provider>
-      </NavigationContainer>
-    );
-  }
+function App() {
+  return (
+    <NavigationContainer>
+      <Provider store={store}>
+        <Stack.Navigator screenOptions={{headerShown:false}}>
+          <Stack.Screen name="Login" component={LoginScreen} />
+          <Stack.Screen name="Profile" component={Profile} />
+        </Stack.Navigator>
+      </Provider>
+    </NavigationContainer>
+  );
 }
 
 export default App;
