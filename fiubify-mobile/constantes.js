75dd177f196f0fe48f576428aede1633d101af9a
NachitoPerez <irperez@fi.uba.ar>
2022-06-16 00:38:15 -0300

    feat: added constants file

---
+++ fiubify-mobile/constantes.js
@@ -0,0 +1,11 @@
+export const userMetricsUrl = "https://fiubify-middleware-staging.herokuapp.com/metrics/users/events";
+export const loginAction = "Login";
+export const signupAction = "Signup";
+export const passwordAction = "Password";
+export const federatedTypeAction = "Federated";
+export const emailTypeAction = "Email";
+export const resetTypeAction = "Reset";
+
+export const contentMetricsUrl = "https://fiubify-middleware-staging.herokuapp.com/metrics/contents/events";
+
+
