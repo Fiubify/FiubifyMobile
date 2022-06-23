305a0130f1db542970b1f60035ef9f1b574dd069
Roberto Ezequiel Iskandarani <64543556+Robert-Iskandarani@users.noreply.github.com>
2022-06-23 10:37:43 -0300

    se estiliza la vista de canciones

--- fiubify-mobile/components/Screens/AllSongs.js
+++ fiubify-mobile/components/Screens/AllSongs.js
@@ -3,15 +3,24 @@ import { StyleSheet, Text, View } from "react-native";
 import { postSongEvent } from "../../src/fetchMetrics";
 import { listenedAction } from "../../constantes";
 import { getAlbumById } from "../../src/fetchContent";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 
 function ListedSong({ song, onPress, userUId }) {
   return (
-    <UiButton
-      pressableStyle={styles.songs}
-      textStyle={styles.songsText}
-      title={song.title}
-      onPress={() => onPress(song)}
-    ></UiButton>
+    <View style={styles.musicContainer}>
+      <MaterialCommunityIcons
+        style={styles.musicIcon}
+        name="music"
+        size={20}
+        color="white"
+      />
+      <UiButton
+        pressableStyle={styles.songs}
+        textStyle={styles.songsText}
+        title={song.title}
+        onPress={() => onPress(song)}
+      ></UiButton>
+    </View>
   );
 }
 
@@ -31,9 +40,7 @@ export function AllSongs({ setSong, songs, currentUserUId }) {
         </View>
       );
     } else {
-      return (
-        <Text>No Content available</Text>
-      );
+      return <Text>No Content available</Text>;
     }
   } else {
     return null;
@@ -46,11 +53,30 @@ const styles = StyleSheet.create({
     alignItems: "center",
     display: "flex",
   },
-  songs: {
+  musicContainer: {
+    width: "90%",
+    marginTop: 20,
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
     backgroundColor: "white",
     borderColor: "#006E95",
     borderWidth: 2,
-    marginTop: 20,
+    borderRadius: 25,
+  },
+  musicIcon: {
+    backgroundColor: "#006E95",
+    borderRadius: 25,
+    padding: "3%",
+    display: "flex",
+    textAlign: "center",
+    textAlignVertical: "center",
+  },
+  songs: {
+    width: "85%",
+    backgroundColor: "white",
+    elevation: 0,
   },
   songsText: {
     color: "#006E95",
